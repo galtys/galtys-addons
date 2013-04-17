d8464503089d29f62696c1ecae72aca796e58afb
jan <jan@asus>
2013-04-17 12:27:33 +0100

    added wizard to open entries

--- budget_report/budget_entries_report.py
+++ budget_report/budget_entries_report.py
@@ -1,6 +1,59 @@
 from openerp import tools
 from openerp.osv import fields,osv
 
+
+class budget_entries_wizard(osv.osv_memory):
+    _name = "budget.entries.wizard"
+    _description = "Open Entries Wizard"
+    _columns = {
+        'name': fields.char('Name'),
+       }
+
+    def action_open_entries(self, cr, uid, ids, context=None):
+        print 'update pricelist', ids, context
+        active_ids=context['active_ids']
+        for w in self.browse(cr, uid, ids):
+            br_items=self.pool.get('budget.entries.report').browse(cr, uid, active_ids)
+            acc_ids=[br.account_id.id for br in br_items]
+            gen_ids=[[x.id for x in br.general_budget_id.account_ids] for br in br_items]
+            month_ids=[br.month for br in br_items]
+            print acc_ids, gen_ids
+            #['|',('create_date','>', self.delta),('write_date','>', self.delta),
+
+            args=[('account_id','in',acc_ids),
+                  ('general_account_id','in',reduce(lambda x,y:x+y,gen_ids))]
+            
+            line_ids=self.pool.get('account.analytic.line').search(cr, uid, args)
+            line_str=','.join( map(str,line_ids))
+            cr.execute("select id,to_char(date::timestamp with time zone, 'MM'::text) AS month from account_analytic_line where id in (%s)"%(line_str))
+            item_id_month=[x for x in cr.fetchall()]
+            print 'filter months', item_id_month, month_ids
+            out_ids=[]
+            for line_id, month in item_id_month:
+                print 'line, month', line_id, month
+                if month in month_ids:
+                    out_ids.append(line_id)
+                else:
+                    print 'not in selection', line_id, month, month_ids
+            print 'out_ids', out_ids
+            #open_lines= [x[0] for x in cr.fetchall() if x[1] in month_ids]
+            #print 'open lines', open_lines
+            #line_ids=self.pool.get('account.analytic.line').search(cr, uid, [])
+            #print 'account line items', line_ids
+        return {
+            'domain': "[('id','in', [" + ','.join(map(str, out_ids)) + "])]",
+            
+            #'domain': str(args),
+            'name': 'Analytic Items',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'account.analytic.line',
+            #'view_id': False,
+            'type': 'ir.actions.act_window',
+            #'search_view_id': id['res_id']
+        }
+budget_entries_wizard()
+
 class budget_entries_report(osv.osv):
     _name = "budget.entries.report"
     _description = "Budget Entries Statistics"
