697c495f48bc0cb119c4df020ca85aa958d5cb3c
jan <jan@asus>
2013-06-21 10:24:18 +0100

    new sql

--- budget_report/budget_entries_report.py
+++ budget_report/budget_entries_report.py
@@ -75,40 +75,7 @@ class budget_entries_wizard(osv.osv_memory):
             #'search_view_id': id['res_id']
         }
 budget_entries_wizard()
-
-class budget_entries_report(osv.osv):
-    _name = "budget.entries.report"
-    _description = "Budget Entries Statistics"
-    _auto = False
-    _columns = {
-        #'date': fields.date('Date', readonly=True),
-        'year': fields.char('Year', size=4, readonly=True),
-        #'day': fields.char('Day', size=128, readonly=True),
-        'month':fields.selection([('01','January'), ('02','February'), ('03','March'), ('04','April'),
-            ('05','May'), ('06','June'), ('07','July'), ('08','August'), ('09','September'),
-            ('10','October'), ('11','November'), ('12','December')], 'Month',readonly=True),
-        #'user_id': fields.many2one('res.users', 'User',readonly=True),
-        #'name': fields.char('Description', size=64, readonly=True),
-        #'partner_id': fields.many2one('res.partner', 'Partner'),
-        'company_id': fields.many2one('res.company', 'Company', required=True),
-        #'currency_id': fields.many2one('res.currency', 'Currency', required=True),
-        'account_id': fields.many2one('account.analytic.account', 'Account', required=False),
-        #'general_account_id': fields.many2one('account.account', 'General Account', required=True),
-        #'journal_id': fields.many2one('account.analytic.journal', 'Journal', required=True),
-        #'move_id': fields.many2one('account.move.line', 'Move', required=True),
-        #'product_id': fields.many2one('product.product', 'Product', required=True),
-        #'product_uom_id': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
-        'crossovered_budget_id': fields.many2one('crossovered.budget', 'Budget', required=False),
-        'general_budget_id': fields.many2one('account.budget.post', 'Budget Position', required=False),
-        'amount': fields.float('Amount', readonly=True),
-        'planned_amount': fields.float('Planned Amount', readonly=True),
-        'unit_amount': fields.float('Quantity', readonly=True),
-        'variance': fields.float('Variance', readonly=True),
-        'nbr': fields.integer('#Entries', readonly=True),
-    }
-    def init(self, cr):
-        tools.drop_view_if_exists(cr, 'budget_entries_report')
-        cr.execute("""
+sql_18apr="""
 CREATE OR REPLACE VIEW budget_entries_report AS 
  SELECT min(a.id) AS id,
         count(DISTINCT a.id) AS nbr,
@@ -151,7 +118,87 @@ UNION
           WHERE aal.account_id = cbl.analytic_account_id AND abr.budget_id = abp.id AND aal.general_account_id = abr.account_id AND aal.date >= cbl.date_from AND aal.date <= cbl.date_to))
   GROUP BY to_char(cbl.date_from::timestamp with time zone, 'YYYY'::text), to_char(cbl.date_from::timestamp with time zone, 'MM'::text),
            cbl.company_id, rc.currency_id, cbl.analytic_account_id, cbl.crossovered_budget_id, cbl.general_budget_id;
-            """)
+            """
+
+sql_21jun="""
+CREATE OR REPLACE VIEW budget_entries_report AS 
+ SELECT min(a.id) AS id,
+        count(DISTINCT a.id) AS nbr,
+        to_char(a.date::timestamp with time zone, 'YYYY'::text) AS year,
+        to_char(a.date::timestamp with time zone, 'MM'::text) AS month,
+        a.company_id,
+        rc.currency_id,
+        a.account_id,
+        cbl.crossovered_budget_id,
+        cbl.general_budget_id,
+        sum(a.amount) AS amount,
+        sum(a.unit_amount) AS unit_amount,
+        round(sum(cbl.planned_amount) / count(DISTINCT a.id)::numeric, 2) AS planned_amount,
+        case when round(sum(cbl.planned_amount) / count(DISTINCT a.id)::numeric, 2) = 0 then 0 else round(sum(a.amount) / (round(sum(cbl.planned_amount) / count(DISTINCT a.id)::numeric, 2) / 100::numeric), 0) end AS variance
+   FROM account_analytic_line a, account_analytic_account analytic, crossovered_budget cb, crossovered_budget_lines cbl, account_budget_post abp, res_company rc
+  WHERE analytic.id = a.account_id AND abp.id = cbl.general_budget_id AND cb.id = cbl.crossovered_budget_id AND cbl.analytic_account_id = a.account_id
+    AND a.date >= cbl.date_from AND a.date <= cbl.date_to AND rc.id = cbl.company_id 
+    AND (EXISTS ( SELECT 'X'
+           FROM account_budget_rel abr
+          WHERE abr.budget_id = abp.id AND a.general_account_id = abr.account_id))
+  GROUP BY to_char(a.date::timestamp with time zone, 'YYYY'::text), to_char(a.date::timestamp with time zone, 'MM'::text), a.company_id, rc.currency_id, a.account_id, cbl.crossovered_budget_id, cbl.general_budget_id
+UNION
+ SELECT min(cbl.id) AS id,
+        0 AS nbr,
+        to_char(cbl.date_from::timestamp with time zone, 'YYYY'::text) AS year,
+        to_char(cbl.date_from::timestamp with time zone, 'MM'::text) AS month,
+        cbl.company_id,
+        rc.currency_id,
+        cbl.analytic_account_id,
+        cbl.crossovered_budget_id,
+        cbl.general_budget_id,
+        0 AS amount,
+        0 AS unit_amount,
+        sum(cbl.planned_amount) AS planned_amount, 
+        0 AS variance
+   FROM account_analytic_account analytic, crossovered_budget cb, crossovered_budget_lines cbl, account_budget_post abp, res_company rc
+  WHERE analytic.id = cbl.analytic_account_id AND abp.id = cbl.general_budget_id AND cb.id = cbl.crossovered_budget_id AND rc.id = cbl.company_id
+    AND (not EXISTS ( SELECT 'X'
+           FROM account_analytic_line aal, account_budget_rel abr
+          WHERE aal.account_id = cbl.analytic_account_id AND abr.budget_id = abp.id AND aal.general_account_id = abr.account_id AND aal.date >= cbl.date_from AND aal.date <= cbl.date_to))
+  GROUP BY to_char(cbl.date_from::timestamp with time zone, 'YYYY'::text), to_char(cbl.date_from::timestamp with time zone, 'MM'::text),
+           cbl.company_id, rc.currency_id, cbl.analytic_account_id, cbl.crossovered_budget_id, cbl.general_budget_id;
+
+"""
+
+class budget_entries_report(osv.osv):
+    _name = "budget.entries.report"
+    _description = "Budget Entries Statistics"
+    _auto = False
+    _columns = {
+        #'date': fields.date('Date', readonly=True),
+        'year': fields.char('Year', size=4, readonly=True),
+        #'day': fields.char('Day', size=128, readonly=True),
+        'month':fields.selection([('01','January'), ('02','February'), ('03','March'), ('04','April'),
+            ('05','May'), ('06','June'), ('07','July'), ('08','August'), ('09','September'),
+            ('10','October'), ('11','November'), ('12','December')], 'Month',readonly=True),
+        #'user_id': fields.many2one('res.users', 'User',readonly=True),
+        #'name': fields.char('Description', size=64, readonly=True),
+        #'partner_id': fields.many2one('res.partner', 'Partner'),
+        'company_id': fields.many2one('res.company', 'Company', required=True),
+        #'currency_id': fields.many2one('res.currency', 'Currency', required=True),
+        'account_id': fields.many2one('account.analytic.account', 'Account', required=False),
+        #'general_account_id': fields.many2one('account.account', 'General Account', required=True),
+        #'journal_id': fields.many2one('account.analytic.journal', 'Journal', required=True),
+        #'move_id': fields.many2one('account.move.line', 'Move', required=True),
+        #'product_id': fields.many2one('product.product', 'Product', required=True),
+        #'product_uom_id': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
+        'crossovered_budget_id': fields.many2one('crossovered.budget', 'Budget', required=False),
+        'general_budget_id': fields.many2one('account.budget.post', 'Budget Position', required=False),
+        'amount': fields.float('Amount', readonly=True),
+        'planned_amount': fields.float('Planned Amount', readonly=True),
+        'unit_amount': fields.float('Quantity', readonly=True),
+        'variance': fields.float('Variance', readonly=True),
+        'nbr': fields.integer('#Entries', readonly=True),
+    }
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'budget_entries_report')
+        cr.execute(sql_21jun)
 
 budget_entries_report()
 
