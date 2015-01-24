9737318e996faf27ee787d7889b128f22f0a3898
Jan Troler <jan.troler@galtys.com>
2015-01-24 16:05:33 +0000

    tidy

--- deployments/__openerp__.py
+++ deployments/__openerp__.py
@@ -6,19 +6,19 @@
     "website" : "http://www.galtys.com/",
     "category" : "Generic Modules/Others",
     "depends" : ['galtys_deploy'],
-    "description" : "DATA",
+    "description" : "ui",
     "init_xml" : [],
     "demo_xml" : [],
     "data" : [],
-    "update_xml" : ['data.xml','galtys_deploy_view.xml',
-                    
-                    'data/deploy.host.group.csv',
-                    'data/deploy.host.user.csv',
+    "update_xml" : ['galtys_deploy_view.xml',
+                    'data.xml',                    
+#                    'data/deploy.host.group.csv',
+ #                   'data/deploy.host.user.csv',
 
                     #'launchpad.xml'
-                    'data/deploy.repository.csv',
-                    'data/deploy.application.csv',
-                    'data/deploy.mako.template.csv'],        
+  #                  'data/deploy.repository.csv',
+   #                 'data/deploy.application.csv',
+    #                'data/deploy.mako.template.csv'],        
     "active": False,
     "installable": True
 }
