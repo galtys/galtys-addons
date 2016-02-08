bdca0a1c97611cff6e6fa73151e1ac28c92ff2d2
Jan <accounts@galtys.com>
2016-02-08 10:54:51 +0000

    make form views wide

---
+++ openerp7_wide_form/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    "name":"OpenERP 7 wide forms",
+    "description":"OpenERP 7 wide forms",
+    "version":"1.0",
+    "author":"jan.troler@galtys.com",
+    "depends":["web"],
+    "css":["base.css"],
+}
