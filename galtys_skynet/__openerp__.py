54ab28166f9e0071b8c5c5af0748547bf37ed247
Jan Troler <jan.troler@galtys.com>
2017-08-09 22:24:39 +0100

    galtys_skynet: add __openerp__.py manifest for v7

---
+++ galtys_skynet/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 *-*
+{
+    "name" : "galtys_skynet",
+    "version" : "0.1draft",
+    "author" : "Jan Troler",
+    "website" : "http://www.galtys.com/",
+    "category" : "Generic Modules/Others",
+    "depends" : ['base'],
+    "description" : "Skynet Experiment :-)",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data" : [],
+    "update_xml" : [],
+    "active": False,
+    "installable": True
+}
