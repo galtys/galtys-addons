6a9f7c627271718d4ac7002e9c6ae58247a59fcf
Jan Troler <jan.troler@galtys.com>
2018-01-26 17:56:39 +0000

    moved protlib to galtys_skynet, tmp

--- galtys_skynet/models/skynet70.py
+++ galtys_skynet/models/skynet70.py
@@ -4,7 +4,9 @@ from openerp.osv import fields, osv
 from lxml import etree
 
 import datetime
-    
+import odoo2proto
+from odoo2proto import odoo2pbmsg
+
 class SkynetSettings(osv.osv):
     _name = "skynet.settings"
     _description = "Skynet Settings"
@@ -15,6 +17,7 @@ class SkynetSettings(osv.osv):
 
 class SkynetModel(osv.osv):
     _name = "skynet.schema.model"
+    _order = "sequence"
     _columns = {
         'name':fields.char("name"),
         'sequence':fields.integer("sequence"),
@@ -28,7 +31,17 @@ class SkynetSchema(osv.osv):
 
     _columns={
         'name':fields.char("Name"),
+        'registry_json':fields.text("Registry Json"),
         'model_ids':fields.one2many("skynet.schema.model","schema_id","Schema Model"),
     }
 
+    def store_registry_json(self, cr, uid, ids, context=None):
+
+        for schema in self.browse(cr, uid, ids):
+            models = []
+            for sm in schema.model_ids:
+                models.append( sm.model_id.model )
+            registry_json = odoo2pbmsg(self.pool, cr, uid, models)
+            schema.write( {"registry_json":registry_json} )
+        
 
